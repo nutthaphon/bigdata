ffc8b46b34f02f7bf846b30988a5c2c3877c7711
Nutthaphon Suwanwong <nutthaphon.su@gmail.com>
2019-07-29 14:04:40 +0700

    Add new Oozie, Livy, Pig & Maven.

--- hadoop-3.1.2-cluster/hdmaster/start.sh
+++ hadoop-3.1.2-cluster/hdmaster/start.sh
@@ -3,10 +3,4 @@ sbin/start-dfs.sh
 sbin/start-yarn.sh
 bin/mapred --daemon start historyserver
 
-echo "Show services"
-echo "[ hdmaster ]"
-ssh hdmaster /java/bin/jps
-echo "[ hdslave1 ]"
-ssh hdslave1 /java/bin/jps
-echo "[ hdslave2 ]"
-ssh hdslave2 /java/bin/jps
+./status.sh
