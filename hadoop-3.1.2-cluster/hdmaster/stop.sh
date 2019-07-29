e4a6ca1e4e2b9763ef0f86e51d55c1d3a03f81f5
Nutthaphon Suwanwong <nutthaphon.su@gmail.com>
2019-07-29 14:47:28 +0700

    Add new Oozie, Livy, Pig & Maven.

--- hadoop-3.1.2-cluster/hdmaster/stop.sh
+++ hadoop-3.1.2-cluster/hdmaster/stop.sh
@@ -3,10 +3,4 @@ sbin/stop-yarn.sh
 #bin/yarn --daemon stop proxyserver
 sbin/stop-dfs.sh
 
-echo "Show services"
-echo "[ hdmaster ]"
-ssh hdmaster /java/bin/jps
-echo "[ hdslave1 ]"
-ssh hdslave1 /java/bin/jps
-echo "[ hdslave2 ]"
-ssh hdslave2 /java/bin/jps
+./status.sh
