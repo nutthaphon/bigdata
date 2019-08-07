1bc9c1587c76f9240e64a82a65cd01e14a6986a7
Nutthaphon Suwanwong <nutthaphon.su@gmail.com>
2019-08-07 15:12:13 +0700

    Oozie completed.

--- hadoop-3.1.2-cluster/hdmaster/start.sh
+++ hadoop-3.1.2-cluster/hdmaster/start.sh
@@ -1,6 +1,9 @@
+echo -e "\tStart sync configuration files.\n"
+./sync.sh
+echo -e "\tStart up hadoop cluster.\n"
 sbin/start-dfs.sh
 #bin/yarn --daemon start proxyserver
 sbin/start-yarn.sh
 bin/mapred --daemon start historyserver
-
+echo -e "\tCheck up services.\n"
 ./status.sh
--- hadoop-3.1.2-cluster/hdmaster/sync.sh
+++ hadoop-3.1.2-cluster/hdmaster/sync.sh
@@ -1,3 +1,7 @@
 grep -v localhost etc/hadoop/workers | while read node; do
-scp etc/hadoop/* ${node}:etc/hadoop
+	echo -e "\t${node} start sync..."
+	scp etc/hadoop/* ${node}:etc/hadoop
+	scp .bashrc ${node}:.bashrc
+	scp .profile ${node}:.profile
+	echo -e "\t${node} synced.\n"
 done
