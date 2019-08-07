47716cafaddad197c24fda0014dfb2ca18bd3f18
Nutthaphon Suwanwong <nutthaphon.su@gmail.com>
2019-08-07 15:12:13 +0700

    Oozie completed.

--- hadoop-3.1.2-cluster/hdmaster/stop.sh
+++ hadoop-3.1.2-cluster/hdmaster/stop.sh
@@ -1,6 +1,7 @@
+echo -e "\tShutdown hadoop cluster.\n"
 bin/mapred --daemon stop historyserver
 sbin/stop-yarn.sh
 #bin/yarn --daemon stop proxyserver
 sbin/stop-dfs.sh
-
+echo -e "Check up services"
 ./status.sh
